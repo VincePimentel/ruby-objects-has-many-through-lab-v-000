716d09e47f09a96d0600aef1e3a54471523460ab
Vince Andrew Pimentel <contact@vincep.me>
2018-10-26 02:07:41 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -13,14 +13,10 @@ class Genre
   end
 
   def songs
-    Song.all.select do |song|
-      song.genre == self
-    end
+    Song.all.select { |song| song.genre == self }
   end
 
   def artists
-    songs.map do |song|
-      song.artist
-    end
+    songs.map { |song| song.artist }
   end
 end
